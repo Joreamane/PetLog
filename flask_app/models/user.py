58eae19bff6fe67a42b3296c0fb1e933f3782f5d
Joreamane <ericmfal@gmail.com>
2023-05-18 18:52:41 -0700

    User methods complete

--- flask_app/models/user.py
+++ flask_app/models/user.py
@@ -1,4 +1,5 @@
-from flask_app.config.mysqlconnection import MySQLConnection
+from flask_app.config.mysqlconnection import connectToMySQL
+from flask import flash
 
 #regex for email validation
 import re
@@ -13,3 +14,46 @@ class User:
         self.password = data['password']
         #blank array to store all user's pets
         self.pets = []
+
+    @classmethod
+    def add_user(cls,data):
+        query = 'INSERT INTO users (first_name, last_name, email, password, created_at, updated_at) VALUES (%(first_name)s, %(last_name)s, %(email)s, %(password)s, NOW(), NOW());'
+        return connectToMySQL('pet_log').query_db(query,data)
+    
+    @classmethod
+    def get_user(cls,data):
+        query = 'SELECT * FROM users WHERE id = %(id)s;'
+        return connectToMySQL('pet_log').query_db(query,data)
+    
+    @classmethod
+    def get_by_email(cls,data):
+        query = 'SELECT * FROM users WHERE email = %(email)s;'
+        return connectToMySQL('pet_log').query_db(query,data)
+    
+    @staticmethod:
+    def validate_registration(user):
+        is_valid = True
+        if not EMAIL_REGEX.match(user['email']):
+            flash('Please enter a valid email address.', 'register')
+            is_valid = False
+        if len(user['first_name']) < 2:
+            flash('First name must be at least 2 characters', 'register')
+            is_valid = False
+        if len(user['last_name']) < 2:
+            flash('Last name must be at least 2 characters', 'register')
+            is_valid = False
+        if user['regpassword'] != user['confirm']:
+            flash('Passwords must match', 'register')
+            is_valid = False
+        if len(user['regpassword']) < 8:
+            flash('Password must be at least 8 characters', 'register')
+            is_valid = False
+        return is_valid
+    
+    @staticmethod
+    def validate_login(user):
+        is_valid = True
+        if not EMAIL_REGEX.match(user['email']):
+            flash('Please enter a valid email address.', 'login')
+            is_valid = False
+        return is_valid
