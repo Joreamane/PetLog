72c64161975e860ce2bca684055e07e060ec8e24
Eric Fallon <ericmfal@gmail.com>
2023-05-28 16:00:42 -0700

    Login route

--- flask_app/controllers/routes.py
+++ flask_app/controllers/routes.py
@@ -35,3 +35,18 @@ def register():
     session['first_name'] = data['first_name']
     session['last_name'] = data['last_name']
     return redirect('/dashboard')
+
+@app.route('/login', methods=['post'])
+def login():
+    data = {'email':request.form['email']}
+    user_in_db = User.get_by_email(data)
+    if not user_in_db:
+        flash('Email does not have an associated account.', 'login')
+        return redirect('/')
+    if not bcrypt.check_password_hash(user_in_db.password, request.form['password']):
+        flash('Invalid email/password combination.', 'login')
+        return redirect('/')
+    session['user_id'] = user_in_db.id
+    session['first_name'] = user_in_db.first_name
+    session['last_name'] = user_in_db.last_name
+    return redirect('/dashboard')
