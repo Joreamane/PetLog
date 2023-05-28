4060c1afe3a9a175f8427de4e87a0b870202cd91
Eric Fallon <ericmfal@gmail.com>
2023-05-27 17:28:45 -0700

    Register route

--- flask_app/controllers/routes.py
+++ flask_app/controllers/routes.py
@@ -1,6 +1,29 @@
+from flask_bcrypt import Bcrypt
 from flask_app import app
 from flask import render_template, redirect, request, session, flash
+from flask_app.models.user import User
+
+import os
+bcrypt = Bcrypt(app)
 
 @app.route('/')
 def index():
     return render_template('login.html')
+
+@app.route('/register', methods=['post'])
+def register():
+    pw_hash = bcrypt.generate_password_hash(request.form['regpassword'])
+    print(pw_hash)
+    data = {
+        'first_name' : request.form['first_name'],
+        'last_name': request.form['last_name'],
+        'email': request.form['email'],
+        'password': pw_hash,
+    }
+    if not User.validate_registration(request.form):
+        return redirect('/')
+    user_id = User.add_user(data)
+    session['user_id'] = user_id
+    session['first_name'] = data['first_name']
+    session['last_name'] = data['last_name']
+    return redirect('/dashboard')
