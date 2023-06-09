c4c1ab53b9952ac06d142161bdefe688ca12ea88
Eric Fallon <ericmfal@gmail.com>
2023-06-08 19:18:53 -0700

    Working on image upload

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from werkzeug.utils import secure_filename
 
-UPLOAD_FOLDER = ''
+UPLOAD_FOLDER = 'flask_app\static\petImages'
+ALLOWED_EXTENSIONS = { 'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER']
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.secret_key = 'fallon'
--- flask_app/controllers/petroutes.py
+++ flask_app/controllers/petroutes.py
@@ -2,6 +2,21 @@ from flask_app import app
 from flask import render_template, redirect, request, session, flash
 from flask_app.models.user import User
 from flask_app.models.pet import Pet
+from werkzeug.utils import secure_filename
+from flask_app import ALLOWED_EXTENSIONS
+
+import os
+
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 
 @app.route('/newpet')
 def newPet_page():
@@ -17,7 +32,9 @@ def newPet():
         'pet_name': request.form['pet_name'],
         'pet_type': request.form['pet_type'],
         'pet_breed': request.form['pet_breed'],
+        'image_path': request.form['image_path'],
         'user_id': session['user_id'],
     }
+    upload_file(data.image_path)
     Pet.add_pet(data)
     return redirect('/dashboard')
