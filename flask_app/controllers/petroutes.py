ca6e7c41a7760812cdd4027147509c0533ab666f
Eric Fallon <ericmfal@gmail.com>
2023-06-08 17:01:34 -0700

    Add pet page and route

--- flask_app/controllers/petroutes.py
+++ flask_app/controllers/petroutes.py
@@ -3,3 +3,21 @@ from flask import render_template, redirect, request, session, flash
 from flask_app.models.user import User
 from flask_app.models.pet import Pet
 
+@app.route('/newpet')
+def newPet_page():
+    if not session:
+        flash('Please log in first!', 'login')
+        return redirect('/')
+    data = {'id': session['user_id']}
+    return render_template('newpet.html', user=User.get_user(data))
+
+@app.route('/newpet', methods=['POST'])
+def newPet():
+    data = {
+        'pet_name': request.form['pet_name'],
+        'pet_type': request.form['pet_type'],
+        'pet_breed': request.form['pet_breed'],
+        'user_id': session['user_id'],
+    }
+    Pet.add_pet(data)
+    return redirect('/dashboard')
